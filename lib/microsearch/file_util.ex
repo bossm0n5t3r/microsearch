47944045c47bd3caa1da0663ad9961af9d80cdd9
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-04-12 21:10:00 +0900

    feat: Add file_util.ex

---
+++ lib/microsearch/file_util.ex
@@ -0,0 +1,15 @@
+defmodule Microsearch.FileUtil do
+  @spec read_lines(String.t()) :: list(String.t())
+  def read_lines(filename) do
+    path = Path.expand(filename, __DIR__)
+
+    case File.read(path) do
+      {:ok, lines} ->
+        lines |> String.split("\n", trim: true)
+
+      {:error, _} ->
+        IO.puts("Error: #{filename} file not found")
+        System.halt(1)
+    end
+  end
+end
