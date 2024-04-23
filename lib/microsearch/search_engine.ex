1aa49e0156280e41c5b1ff05e36603e9a72f3b55
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-04-24 04:07:10 +0900

    feat: Add Microsearch.SearchEngine.bulk_index
    - Add search_engine.ex

---
+++ lib/microsearch/search_engine.ex
@@ -0,0 +1,43 @@
+defmodule Microsearch.SearchEngine do
+  alias Microsearch.FileUtil
+
+  def bulk_index() do
+    # Read parquet file
+    dataframe = FileUtil.read_parquet_to_dataframe("./output.parquet")
+
+    # Create documents (zip with url and content)
+    urls = dataframe[:url] |> Explorer.Series.to_list()
+    contents = dataframe[:content] |> Explorer.Series.to_list()
+    documents = Enum.zip(urls, contents)
+
+    for {url, content} <- documents do
+      index(url, content)
+    end
+  end
+
+  defp index(url, content) do
+    Cachex.put(:documents, url, content)
+
+    words =
+      normalize_string(content)
+      |> String.split(" ", trim: true)
+
+    for word <- words do
+      Cachex.get_and_update(:index, "#{word}_#{url}", fn
+        nil -> 1
+        val -> val + 1
+      end)
+    end
+
+    has_avdl = Cachex.exists?(:avdl, "avdl") |> elem(1)
+
+    if has_avdl do
+      Cachex.del(:avdl, "avdl")
+    end
+  end
+
+  defp normalize_string(input_string) do
+    String.replace(input_string, ~r/[[:punct:]]/, " ")
+    |> String.downcase()
+  end
+end
