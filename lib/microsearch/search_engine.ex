344f463aca5757b2cd4a6110b1bf340aac778e73
bossm0n5t3r <bossm0n5t3r@gmail.com>
2024-04-28 03:31:43 +0900

    feat: Add Microsearch.SearchEngine.search

--- lib/microsearch/search_engine.ex
+++ lib/microsearch/search_engine.ex
@@ -58,4 +58,81 @@ defmodule Microsearch.SearchEngine do
     String.replace(input_string, ~r/[[:punct:]]/, " ")
     |> String.downcase()
   end
+
+  def search(query) do
+    normalize_string(query)
+    |> String.split(" ", trim: true)
+    |> Enum.reduce(%{}, fn keyword, acc ->
+      kw_urls_score = bm25(keyword)
+      update_url_scores(acc, kw_urls_score)
+    end)
+  end
+
+  defp bm25(keyword) do
+    idf_score = idf(keyword)
+    avdl_value = avdl()
+
+    get_urls(keyword)
+    |> Enum.reduce(%{}, fn {url, freq}, acc ->
+      numerator = freq * (1.5 + 1)
+      denominator = freq + 1.5 * (1 - 0.75 + 0.75 * get_document_length(url) / avdl_value)
+      Map.put(acc, url, idf_score * numerator / denominator)
+    end)
+  end
+
+  defp idf(keyword) do
+    n = number_of_documents()
+    n_kw = get_urls(keyword) |> Enum.count()
+    Math.log((n - n_kw + 0.5) / (n_kw + 0.5) + 1)
+  end
+
+  defp get_urls(keyword) do
+    normalized_keyword = normalize_string(keyword)
+
+    Cachex.get(:index, normalized_keyword)
+    |> elem(1)
+  end
+
+  def posts() do
+    Cachex.keys(:documents)
+    |> elem(1)
+  end
+
+  defp number_of_documents() do
+    posts() |> Enum.count()
+  end
+
+  defp avdl() do
+    Cachex.get(:avdl, "avdl")
+    |> case do
+      {:ok, nil} ->
+        advl_value =
+          Enum.map(posts(), fn url -> get_document_length(url) end)
+          |> Enum.sum()
+          |> Kernel.div(number_of_documents())
+
+        Cachex.put(:avdl, "avdl", advl_value)
+        advl_value
+
+      {:ok, avdl} ->
+        avdl
+    end
+  end
+
+  defp get_document_length(url) do
+    Cachex.get(:documents, url)
+    |> elem(1)
+    |> String.length()
+  end
+
+  defp update_url_scores(old, new) do
+    for {url, score} <- new do
+      if Map.has_key?(old, url) do
+        {url, old[url] + score}
+      else
+        {url, score}
+      end
+    end
+    |> Map.new()
+  end
 end
